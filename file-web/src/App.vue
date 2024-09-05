a1d940db02d25043f31637400cd5aaaa8c20fc36
CxyMac <naivy929@outlook.com>
2024-09-05 23:34:55 +0800

    提交

--- file-web/src/App.vue
+++ file-web/src/App.vue
@@ -1,11 +1,5 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">首页</router-link> |
-      <router-link to="/login">登录页面</router-link> |
-      <router-link to="/register">注册页面</router-link>
-      <Header></Header>
-    </div>
     <router-view />
   </div>
 </template>
