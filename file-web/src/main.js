73cc47a649cd35b91ae0853c36a7731e3ba9da87
CxyMac <naivy929@outlook.com>
2024-09-06 14:58:38 +0800

    提交

--- file-web/src/main.js
+++ file-web/src/main.js
@@ -2,13 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-// Element-UI
-import ElementUI from "element-ui";
-import "element-ui/lib/theme-chalk/index.css";
-//
-import "./assets/style/base.styl";
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import './assets/style/base.styl'
+// import '@/router/before.js'
 
-Vue.use(ElementUI);
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 new Vue({
--- file-web/src/store/index.js
+++ file-web/src/store/index.js
@@ -1,15 +1,27 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+import user from "./module/user"; // 引入user.js
+
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    //
+  },
+  getters: {
+    isLogin: (state) => state.user.isLogin,
+    username: (state) => state.user.username,
+    userId: (state) => state.user.userId,
+    userInfoObj: (state) => state.user.userInfoObj,
   },
   mutations: {
+    //
   },
   actions: {
+    //
   },
   modules: {
-  }
-})
+    user,
+  },
+});
