9c6aab60d34311cd1d08dd84ed3b71a8c1ecd46b
wangsiyuan537 <w18722517929@outlook.com>
2024-11-25 11:24:58 +0800

    test

--- src/main/java/com/ski/file/Application.java
+++ src/main/java/com/ski/file/Application.java
@@ -1,20 +1,3 @@
-/*
- *****************************************************************************
- *  Copyright (C), 2023-2024, SUKHOI'Naivy(注册于甘肃云港股份有限公司)
- *
- *  @file    ND1.0
- *  @brief   NetDisk源代码文件不包含(.gitignore)
- *
- *  @author  Naivy
- *  @date    2024.7.1
- *  @version 1.0
- *
- *
- *----------------------------------------------------------------------------
- *  本项目文件代码所有权属于Copyright (C), 2023-2024, SUKHOI'Naivy(注册于甘肃云港股
- *  份有限公司)，未经书面许可，不可使用。
- *****************************************************************************
- */
 
 package com.ski.file;
 
@@ -26,8 +9,8 @@ import org.springframework.boot.autoconfigure.*;
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
 
 }
